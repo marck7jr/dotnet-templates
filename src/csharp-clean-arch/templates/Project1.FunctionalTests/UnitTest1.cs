c20c49bbcf8e8fbf853cdad493bad694a7fc1a8b
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-04-22 13:08:46 -0400

    Add initial project files for templating

---
+++ src/csharp-clean-arch/templates/Project1.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Project1.FunctionalTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/csharp-clean-arch/templates/Project1.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Project1.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
